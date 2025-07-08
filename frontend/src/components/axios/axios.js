5a91d69f48f1946ba963df89b11c0ab12fc12943
Smoke-the-blade <alejandroturiot@gmail.com>
2025-07-08 18:23:41 -0300

    cambios de direccion
    se cambio la direccion local por la direccion de railway

--- frontend/src/components/axios/axios.js
+++ frontend/src/components/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "earnest-nature-production.up.railway.app:3001/",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://earnest-nature-production.up.railway.app/",
 });
 
 export default axiosInstance;
